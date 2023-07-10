c0f8965778a4c5357390b563def042283e86a441
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-07-10 19:40:13 +0100

    chore: mv test function

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,2 @@
-import { httpTransport, emitterFor, CloudEvent, CloudEventV1 } from "cloudevents";
-// https://github.com/cloudevents/sdk-javascript
-
-// Create an emitter to send events to a receiver
-const emit = emitterFor(httpTransport("http://localhost:8080/"));
-
-// Create a new CloudEvent
-const ce: CloudEventV1<string> = {
-  specversion: "1.0",
-  source: "/some/source",
-  type: "example",
-  id: "1234",
-  data: JSON.stringify({ foo: "bar" })
-};
-const event = new CloudEvent(ce);
-
-console.log(event);
-// Send it to the endpoint - encoded as HTTP binary by default
-emit(event);
+// send an example cloudevent to the endpoint
+import "./testCloudEvent";
